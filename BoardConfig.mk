0b2df9f9d9e8e1168bf5393bd8102d107d1ef33e
1nfrag <sil3ntcll@gmail.com>
2018-09-11 21:03:22 +0800

    TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
    Change-Id: I8b0581c5fe04924b194f468182da74a908837cc9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@ TARGET_KERNEL_CONFIG := dipper_defconfig
 
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
+# Fix RIL
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/dipper/BoardConfigVendor.mk
 
